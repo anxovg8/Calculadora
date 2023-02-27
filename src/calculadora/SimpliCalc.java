6e5c5489de9315a4afcf83afee445a0a2ee98725
Anxo Vidal <anxoto@gmail.com>
2023-02-27 14:22:34 +0100

    Commit porquue funciona correctamente el codigo con el switch

--- src/calculadora/SimpliCalc.java
+++ src/calculadora/SimpliCalc.java
@@ -7,41 +7,42 @@ import java.util.Scanner;
  * @author alumno
  */
 public class SimpliCalc {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String entrada;
-        do{
+        do {
             System.out.println("Escoja una operación o pulse 0 para salir.");
             System.out.println("Operaciones: + - * /");
             System.out.print("> ");
             entrada = sc.nextLine();
             int n1 = 0, n2 = 0;
-            if("+-*/".contains(entrada)){
+            if ("+-*/".contains(entrada)) {
                 System.out.print("Operando 1 > ");
                 n1 = sc.nextInt();
                 System.out.print("Operando 2 > ");
                 n2 = sc.nextInt();
-            }else{
+            } else {
                 System.out.println("Operación incorrecta!");
             }
-            if(entrada.equals("+")){
-                System.out.println("%d+%d=%d".formatted(n1,n2,n1+n2));
+            switch (entrada) {
+                case "+":
+                    System.out.println("%d+%d=%d".formatted(n1, n2, n1 + n2));
+                    break;
+                case "-":
+                    System.out.println("%d-%d=%d".formatted(n1, n2, n1 - n2));
+                    break;
+                case "*":
+                    System.out.println("%d*%d=%d".formatted(n1, n2, n1 * n2));
+                    break;
+                case "/":
+                    System.out.println("%d/%d=%d".formatted(n1, n2, n1 / n2));
+                    break;
+
             }
-            
-            if(entrada.equals("-")){
-                System.out.println("%d-%d=%d".formatted(n1,n2,n1-n2));
-            }
-            
-            if(entrada.equals("*")){
-                System.out.println("%d*%d=%d".formatted(n1,n2,n1*n2));
-            }
-            
-            if(entrada.equals("/")){
-                System.out.println("%d/%d=%d".formatted(n1,n2,n1/n2));
-            }
-            
-        }while(!entrada.equals(""));
-        
+
+        } while (!entrada.equals(""));
+
     }
-    
+
 }
